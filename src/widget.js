22353be002d71f36bb6b434f83a21321a16767b1
Rishi Cadambe <44220205+TheIndexx@users.noreply.github.com>
2023-10-09 15:53:27 -0500

    2

--- src/widget.js
+++ src/widget.js
@@ -12,14 +12,41 @@ function openWidget() {
   `;
 
   widget.innerHTML = widgetHTML;
+  widget.style.position = 'fixed';
+  widget.style.bottom = '20px';
+  widget.style.right = '20px';
+  widget.style.backgroundColor = 'green';
+  widget.style.padding = '10px';
+  widget.style.border = '1px solid #ccc';
+  widget.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.2)';
+  widget.style.zIndex = '999';
+
   var closeButton = document.getElementById('close-widget');
   widget.style.display = 'block';
   closeButton.addEventListener('click', closeWidget);
 }
 
 function closeWidget() {
-  widget.style.display = 'none';
-  widget.innerHTML = '';
+  widget.style.position = 'fixed';
+  widget.style.bottom = '20px';
+  widget.style.right = '20px';
+  widget.style.backgroundColor = 'green';
+  widget.style.padding = '10px';
+  widget.style.border = '1px solid #ccc';
+  widget.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.2)';
+  widget.style.zIndex = '999';
+  var widgetHTML = `
+    <div id="custom-widget" class="widget">
+      <div class="widget-content">
+        <button id="open-widget">Open</button>
+      </div>
+    </div>
+  `
+  widget.innerHTML = widgetHTML;
+
+  var openButton = document.getElementById('open-widget');
+  widget.style.display = 'block';
+  openButton.addEventListener('click', openWidget);
 }
 
 document.addEventListener('DOMContentLoaded', function () {
